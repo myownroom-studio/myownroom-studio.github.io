d0e4a5035e58812f9d69bf64133936706059638f
Miran Lee <ranz_@naver.com>
2025-01-11 20:12:59 +0900

    first commit

---
+++ src/layout/Gallery/Images.ts
@@ -0,0 +1,110 @@
+import image01 from '@/assets/images/01.jpeg'
+import image02 from '@/assets/images/02.jpeg'
+import image03 from '@/assets/images/03.jpeg'
+import image04 from '@/assets/images/04.jpeg'
+import image05 from '@/assets/images/05.jpeg'
+import image06 from '@/assets/images/06.jpeg'
+import image07 from '@/assets/images/07.jpeg'
+import image08 from '@/assets/images/08.jpeg'
+import image09 from '@/assets/images/09.jpeg'
+import image10 from '@/assets/images/10.jpeg'
+import image11 from '@/assets/images/11.jpeg'
+import image12 from '@/assets/images/12.jpeg'
+import image13 from '@/assets/images/13.jpeg'
+import image14 from '@/assets/images/14.jpeg'
+import image15 from '@/assets/images/15.jpeg'
+import image16 from '@/assets/images/16.jpeg'
+import image17 from '@/assets/images/17.jpeg'
+
+const images = [   /* 이미지 경로를 리스트로 저장 */
+  {
+    alt: 'image01',
+    source: image01,
+    width: 640,
+    height: 960
+  },
+  {
+    alt: 'image02',
+    source: image02,
+    width: 640,
+    height: 960
+  },
+  {
+    alt: 'image03',
+    source: image03,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image04',
+    source: image04,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image05',
+    source: image05,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image06',
+    source: image06,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image07',
+    source: image07,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image08',
+    source: image08,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image09',
+    source: image09,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image10',
+    source: image10,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image11',
+    source: image11,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image12',
+    source: image12,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image13',
+    source: image13,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image14',
+    source: image14,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image15',
+    source: image15,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image16',
+    source: image16,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image17',
+    source: image17,
+    width: 640,
+    height: 960
+  }
+];
+
+export default images;
